20a9683742ab7bfb824d09442b53f032c99aa222
Adrian Escutia Soto <adrianescutia@yahoo.com.mx>
2024-09-30 12:52:28 -0500

    Removing i18n configuration and fix footer link to docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,10 +21,10 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en'],
+  // },
   markdown: {
     mermaid: true,
   },
@@ -127,8 +127,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Getting Started',
+              to: '/docs/getting-started',
             },
           ],
         },
